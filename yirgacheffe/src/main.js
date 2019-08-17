616f8f3b3ffaedd1f7ec91b4b0bd5cfa3d6199cc
jstzwj <1103870790@qq.com>
2019-08-17 18:47:27 +0800

    add vue router

--- yirgacheffe/src/main.js
+++ yirgacheffe/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+import Albums from './views/Albums.vue'
+
+const routes = [
+  { path: '/', component: Albums },
+  { path: '/Albums', component: Albums },
+]
+
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
